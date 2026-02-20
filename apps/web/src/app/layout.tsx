ca9db266b4e99460e2a7aabee20c3781616a9773
fleddd <fedkiv20172@gmail.com>
2026-02-20 11:47:45 +0200

    changed the font for apps/web

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,11 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata, Viewport } from "next";
+import { Oxanium } from "next/font/google";
 import "../styles/globals.css";
-const inter = Inter({ subsets: ["latin"] });
-
+const oxanium = Oxanium({
+    subsets: ["latin"],
+    weight: ["400", "700", "800"], // Беремо жирні накреслення
+    variable: "--font-oxanium",    // Створюємо CSS змінну
+    display: "swap",
+});
 export const metadata: Metadata = {
     title: "QwikTwik - PC Optimization Utility",
     description: "Maximize FPS and minimize input lag.",
+    appleWebApp: {
+        title: "QwikTwik",
+        statusBarStyle: "black-translucent",
+    },
+};
+
+export const viewport: Viewport = {
+    colorScheme: "dark",
+    themeColor: [
+        { media: "(prefers-color-scheme: light)", color: "#050505" },
+        { media: "(prefers-color-scheme: dark)", color: "#050505" },
+    ],
 };
 
 export default function RootLayout({
@@ -15,7 +31,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="scroll-smooth">
-            <body className={inter.className}>
+            <body className={oxanium.className}>
                 {children}
             </body>
         </html>
